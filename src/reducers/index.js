c59e26086ea7ebd5cc9bf599599b6197ef0c98d6
Philip Hale <philip@pghale.com>
2016-03-16 19:17:28 +0000

    All reducer names end in 'Reducer'

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 const reducers = {
-  applications: require('../reducers/applications.js'),
-  statusFilter: require('../reducers/applicationStatusFilter.js'),
-  searchQuery: require('../reducers/searchApplications.js')
+  applications: require('../reducers/applicationsReducer.js'),
+  statusFilter: require('../reducers/statusFilterReducer.js'),
+  searchQuery: require('../reducers/searchApplicationsReducer.js')
 };
 module.exports = combineReducers(reducers);
