eaab9bce0dad5315734b53fe298be82981ee4348
Philip Hale <philip@pghale.com>
2016-03-16 19:31:29 +0000

    Add back hot loading via HMR, not HMRE
    cf. https://github.com/reactjs/redux/pull/1455

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './stores';
-import App from './containers/App';
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './stores'
 
-const store = configureStore();
+const store = configureStore()
+const rootEl = document.getElementById('app')
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app')
-);
+let render = () => {
+  const App = require('./containers/App').default
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  )
+}
+
+
+if (module.hot) {
+  // Support hot reloading of components
+  // and display an overlay for runtime errors
+  const renderApp = render
+  const renderError = (error) => {
+    const RedBox = require('redbox-react')
+    ReactDOM.render(
+      <RedBox error={error} />,
+        rootEl
+    )
+  }
+  render = () => {
+    try {
+      renderApp()
+    } catch (error) {
+      renderError(error)
+    }
+  }
+  module.hot.accept('./containers/App', () => {
+    setTimeout(render)
+  })
+}
+
+render()
