80b76a4dd75240b765610e7624d0be59b07fdae0
Philip Hale <philip@pghale.com>
2016-03-21 11:40:45 +0000

    Performance improvements for selector

--- src/selectors/getVisibleApplications.js
+++ src/selectors/getVisibleApplications.js
@@ -4,37 +4,33 @@ const getApplications = (state) => state.applications
 const getSearchQuery = (state) => state.searchQuery
 const getStatusFilter = (state) => state.statusFilter
 
-export const getVisibleApplicationsFilteredBySearchQuery = createSelector(
-  [ getApplications, getSearchQuery ],
-  (applications, searchQuery) => {
-    if (searchQuery === undefined) { return applications }
-    return applications.filter((a) => Object.values(a).some(value =>
-      (String(value) === undefined) ? false : String(value).toLowerCase().includes(searchQuery))
-    )
-  }
-)
+const queryMatch = (application, searchQuery) => {
+  if (searchQuery === undefined) { return false }
+  return Object.values(application).some(value =>
+    (String(value) === undefined) ? false : String(value).toLowerCase().includes(searchQuery)
+  )
+}
 
-export const getVisibleApplicationsFilteredByStatus = createSelector(
-  [ getApplications, getStatusFilter ],
-  (applications, status) => {
-    switch (status) {
-      case 'ALL':
-        return applications
-      case 'IN_PROGRESS':
-        return applications.filter(a => a.status == 'In-Progress')
-      case 'SUBMITTED':
-        return applications.filter(a => a.status == 'Submitted')
-      case 'COMPLETED':
-        return applications.filter(a => a.status == 'Completed')
-      default:
-        return applications
-    }
+const statusMatch = (application, status) => {
+  switch (status) {
+    case 'ALL':
+      return true
+    case 'IN_PROGRESS':
+      return application.status === 'In-Progress'
+    case 'SUBMITTED':
+      return application.status === 'Submitted'
+    case 'COMPLETED':
+      return application.status ==='Completed'
+    default:
+      return false
   }
-)
+}
 
 export const getVisibleApplications = createSelector(
-  [ getVisibleApplicationsFilteredBySearchQuery, getVisibleApplicationsFilteredByStatus ],
-  (searchFilteredApplications, statusFilteredApplications) => {
-    return searchFilteredApplications.filter(a => statusFilteredApplications.indexOf(a) != -1)
+  [ getApplications, getStatusFilter, getSearchQuery ],
+  (applications, statusFilter, searchQuery) => {
+    return applications.filter(a =>
+      statusMatch(a, statusFilter) && queryMatch(a, searchQuery)
+    )
   }
 )
